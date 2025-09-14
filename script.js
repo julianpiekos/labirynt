19b53f74a48e92ad38d476aab9e4ac2d296af7eb
Julek <julian.piekos2015@gmail.com>
2025-09-14 16:56:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const elementy = [
     [true, true, true, false, false, false, false, false, false, false],
     [false, false, true, false, true, true, true, false, false, false],
     [false, false, true, false, false, false, true, false, true, true],
-    [false, false, true, true, true, false, true, false, true, true],
+    [false, false, true, true, true, false, true, false, false, false],
     [false, false, false, false, true, false, true, true, true, false],
     [false, false, false, false, true, false, false, false, true, false],
     [false, true, true, true, true, false, false, false, true, false],
@@ -37,3 +37,4 @@ addEventListener("keydown", function (zdarzenie) {
         setTimeout(() => alert("wygrałeś"), 200)
     }
 })
+
